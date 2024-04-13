d965f32ebb8413d614a46636cdf99fca54c008b3
764490519 <liangguoqing777@gmail.com>
2024-04-13 15:56:36 +0200

    task4

---
+++ Assignment2/.history/omp_local_sum_20240413155625.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <math.h>
+
+#define MAX_THREADS 128
+
+double omp_local_sum(double *x, size_t size)
+{
+    double local_sum[MAX_THREADS] = {0};
+    double sum_val = 0.0;
+
+#pragma omp parallel num_threads(MAX_THREADS)
+    {
+        int tid = omp_get_thread_num();
+#pragma omp for
+        for (size_t i = 0; i < size; ++i)
+        {
+            local_sum[tid] += x[i];
+        }
+    }
+
+    for (int i = 0; i < MAX_THREADS; ++i)
+    {
+        sum_val += local_sum[i];
+    }
+
+    return sum_val;
+}
+
+void generate_random(double *input, size_t size)
+{
+    for (size_t i = 0; i < size; ++i)
+    {
+        input[i] = rand() / (double)RAND_MAX;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <number of runs>\n", argv[0]);
+        return 1;
+    }
+
+    int num_runs = atoi(argv[1]);
+    if (num_runs <= 0)
+    {
+        fprintf(stderr, "Number of runs must be a positive integer\n");
+        return 1;
+    }
+
+    size_t size = 10000000; // 10^7
+    double *array = (double *)malloc(size * sizeof(double));
+    generate_random(array, size);
+
+    int thread_counts[] = {1, 32, 64, 128};
+    int num_thread_counts = sizeof(thread_counts) / sizeof(thread_counts[0]);
+
+    for (int j = 0; j < num_thread_counts; ++j)
+    {
+        int num_threads = thread_counts[j];
+        double total_time = 0.0;
+
+        for (int i = 0; i < num_runs; ++i)
+        {
+            double start_time = omp_get_wtime();
+            omp_set_num_threads(num_threads);
+            omp_local_sum(array, size);
+            double end_time = omp_get_wtime();
+            total_time += (end_time - start_time);
+        }
+
+        double average_time = total_time / num_runs;
+        printf("Threads: %d, Average Time: %.6f seconds\n", num_threads, average_time);
+    }
+
+    free(array);
+
+    return 0;
+}
