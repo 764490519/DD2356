878af83555c5d96d2f9a6d94186a9fb04ec3100f
764490519 <liangguoqing777@gmail.com>
2024-05-10 21:59:07 +0200

    timing

--- Assignment3/timing.sh
+++ Assignment3/timing.sh
@@ -1,19 +1,6 @@
-#!/bin/bash
+matrix_dimension=1680
 
-# 定义测试的矩阵大小
-matrix_dimension=256
-
-# 进程数数组，每个元素都应该是一个完全平方数
-# processor_counts=(1 4 9 16 25 36 49 64 100 144 196 225 256)
-processor_sizes = (1)
-
-# # 输出文件
-# OUTPUT_FILE="mpi_performance_results.txt"
-
-# # 清空输出文件
-# echo "Testing MPI Matrix Multiplication Performance" > $OUTPUT_FILE
-# echo "Matrix size: $MATRIX_SIZE" >> $OUTPUT_FILE
-# echo "---------------------------------------------" >> $OUTPUT_FILE
+processor_sizes = (1,4,9,16,25,36,49,64,100,144,196,225,256)
 
 # Loop over each processor size
 for size in "${processor_sizes[@]}"
